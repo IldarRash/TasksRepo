c565d13179ab4aaede913b6a1642ce4979866e76
Shayakhmetov Ildar <Ildar.SHAYAKHMETOV@raiffeisen.ru>
2018-08-16 16:08:17 +0300

    Add timer to update status

--- src/main/java/com/example/demo/repo/TaskRepo.java
+++ src/main/java/com/example/demo/repo/TaskRepo.java
@@ -3,7 +3,10 @@ package com.example.demo.repo;
 import com.example.demo.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TaskRepo extends JpaRepository<Task, UUID> {
+
+    List<Task> findAllByStatus(String status);
 }
--- src/main/java/com/example/demo/services/TaskService.java
+++ src/main/java/com/example/demo/services/TaskService.java
@@ -4,15 +4,19 @@ import com.example.demo.exceptions.NotFoundException;
 import com.example.demo.model.Task;
 import com.example.demo.repo.TaskRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
 public class TaskService {
 
     private final static String STATUS_CREATED = "created";
+    private final static String STATUS_RUNNIG = "running";
+    private final static String STATUS_DONE = "done";
+    private final Long TWO_MIN = 2*60*1000L;
 
     @Autowired
     private TaskRepo taskRepo;
@@ -30,4 +34,17 @@ public class TaskService {
         return taskRepo.findById(id)
                 .orElseThrow(NotFoundException::new);
     }
+
+    @Scheduled(fixedDelay = 1000)
+    private void updateTasks() {
+        List<Task> tasks = taskRepo.findAllByStatus(STATUS_RUNNIG);
+        for (Task task : tasks) {
+            if(System.currentTimeMillis() - task.getTimestamp() >= TWO_MIN) {
+                task.setStatus(STATUS_DONE);
+                task.setTimestamp(System.currentTimeMillis());
+                taskRepo.save(task);
+            }
+        }
+    }
+
 }
